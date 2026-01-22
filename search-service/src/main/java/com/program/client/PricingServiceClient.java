dbc2f51bf0b51a597eb817aee96ee57417873a07
abhyas123 <abhyasparanjiya@gmail.com>
2026-01-22 22:11:16 +0530

    create pricing service client to communicate with pricing service

--- search-service/src/main/java/com/program/client/PricingServiceClient.java
+++ search-service/src/main/java/com/program/client/PricingServiceClient.java
@@ -1,4 +1,20 @@
 package com.program.client;
 
-public class PricingServiceClient {
+import com.program.dto.request.PricingEstimateRequest;
+import com.program.dto.response.VehicleFareResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(
+        name = "pricing-service"
+)
+public interface PricingServiceClient {
+
+    @PostMapping("/pricing/estimate")
+    List<VehicleFareResponse> estimateFare(
+            @RequestBody PricingEstimateRequest request
+    );
 }
