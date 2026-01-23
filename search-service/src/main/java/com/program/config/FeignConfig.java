e738ff0c9506c72b4431c3b33e1b49f673a472e1
abhyas123 <abhyasparanjiya@gmail.com>
2026-01-23 18:52:42 +0530

    add features of map API  and logging using feign config

--- search-service/src/main/java/com/program/config/FeignConfig.java
+++ search-service/src/main/java/com/program/config/FeignConfig.java
@@ -1,4 +1,18 @@
 package com.program.config;
 
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FeignConfig {
+
+    /**
+     * Feign client logging level
+     * BASIC / HEADERS / FULL
+     */
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
--- search-service/src/main/java/com/program/config/MapApiConfig.java
+++ search-service/src/main/java/com/program/config/MapApiConfig.java
@@ -1,4 +1,24 @@
 package com.program.config;
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Getter
+@Configuration
 public class MapApiConfig {
+
+    @Value("${map.api.base-url}")
+    private String baseUrl;
+
+    @Value("${map.api.key}")
+    private String apiKey;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
