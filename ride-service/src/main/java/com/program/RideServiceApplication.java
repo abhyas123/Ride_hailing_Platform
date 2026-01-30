9b067d4181e890ed2c7d3cb00325181767d571de
abhyas123 <abhyasparanjiya@gmail.com>
2026-01-30 22:21:25 +0530

    add ride service module

---
+++ ride-service/src/main/java/com/program/RideServiceApplication.java
@@ -0,0 +1,23 @@
+package com.program;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients
+public class    RideServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideServiceApplication.class, args);
+    }
+}
+
+
+
+
+
+
+
